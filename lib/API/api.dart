f7cc31b8225ece51928e755765ce5629a0c96b9a
Mdshaffat <shaffatbd@gmail.com>
2022-04-13 19:31:46 +0600

    Telemmedicine added

--- lib/API/api.dart
+++ lib/API/api.dart
@@ -1,6 +1,6 @@
 const String ROOTURI = 'outreachhospitaltest.azurewebsites.net';
-// const String ROOT = "https://192.168.0.110:5001/api";
-const String ROOT = "https://hospitalapitest.azurewebsites.net/api";
+const String ROOT = "https://192.168.0.103:5001/api";
+// const String ROOT = "https://hospitalapitest.azurewebsites.net/api";
 const String LOGIN = "$ROOT/account/login";
 const String HOSPITALURI = "$ROOT/hospital/hospitallistsortbyname";
 const String DIVISIONURI = "$ROOT/upazilaanddistrict/division";
@@ -10,3 +10,4 @@ const String ALLDISTRICTURI = "$ROOT/upazilaanddistrict/district";
 const String ALLUPAZILAURI = "$ROOT/upazilaanddistrict/upazila";
 const String PATIENTURI = "$ROOT/patient";
 const String USERS = "$ROOT/userManagement/userlist";
+const String TELEMEDICINEURI = "$ROOT/telemedicine";
--- lib/pages/Users/useList.dart
+++ lib/pages/Users/useList.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:hospital_app/API/api.dart';
 import 'package:hospital_app/Models/patient.dart';
 import 'package:hospital_app/Models/user.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Models/response.dart';
 import '../../utils/app_drawer.dart';
@@ -18,6 +21,9 @@ class UserList extends StatefulWidget {
 }
 
 class _UserListState extends State<UserList> {
+  TextEditingController _patientIdController = new TextEditingController();
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  late ScaffoldMessengerState scaffoldMessenger;
   Paginations? paginations;
   List<String> str = [];
   List<User> _users = [];
@@ -45,20 +51,13 @@ class _UserListState extends State<UserList> {
 
   @override
   Widget build(BuildContext context) {
+    scaffoldMessenger = ScaffoldMessenger.of(context);
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.blue,
-        splashColor: Colors.red,
-        onPressed: () {
-          Navigator.pushReplacementNamed(context, "/patientadd");
-        },
-      ),
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: const Text("User List"),
+        title: const Text("Telemedicine"),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-      drawer: AppDrawer(),
+      drawer: const AppDrawer(),
       body: SingleChildScrollView(
           child: DataTable(
               columns: const <DataColumn>[
@@ -106,7 +105,9 @@ class _UserListState extends State<UserList> {
                                 color: Colors.blue,
                                 splashColor: Colors.purple,
                                 onPressed: () {
-                                  _callNumber(e.phoneNumber.toString());
+                                  _showCallingDialog(e.userId.toString(),
+                                      e.phoneNumber.toString());
+                                  // _callNumber(e.phoneNumber.toString());
                                   // _showMyDialog(e.id);
                                 },
                               ),
@@ -138,7 +139,9 @@ class _UserListState extends State<UserList> {
       for (var item in jsonresponse) {
         users.add(User.fromJson(item));
       }
-      _users = users;
+      setState(() {
+        _users = users;
+      });
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
@@ -146,8 +149,111 @@ class _UserListState extends State<UserList> {
     }
   }
 
-  _callNumber(String phoneNumber) async {
+  Future<bool?> _callNumber(String phoneNumber) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    final String? userId = preferences.getString("id");
     String number = phoneNumber;
-    await FlutterPhoneDirectCaller.callNumber(number);
+    var call = await FlutterPhoneDirectCaller.callNumber(number);
+    return call;
+  }
+
+  _sendCallingInfo(
+    String doctorId,
+    String phoneNumber,
+    int patientId,
+    BuildContext context,
+  ) async {
+    if (_patientIdController.text.isEmpty ||
+        _patientIdController.text == " " ||
+        _patientIdController.text == "") {
+      scaffoldMessenger.showSnackBar(
+          const SnackBar(content: Text("Please Fill Patient ID")));
+    } else {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      final String? userId = preferences.getString("id");
+      var data = jsonEncode(
+          {"patietnId": patientId, "callerId": userId, "receiverId": doctorId});
+
+      Response response;
+      var dio = Dio();
+      response = await dio.post(TELEMEDICINEURI, data: data);
+      if (response.statusCode == 200) {
+        var callSuccess = await _callNumber(phoneNumber);
+        if (callSuccess == false || callSuccess == null) {
+          scaffoldMessenger
+              .showSnackBar(const SnackBar(content: Text("Faild to call")));
+        } else {
+          scaffoldMessenger
+              .showSnackBar(const SnackBar(content: Text("Calling....")));
+          Navigator.of(context).pop();
+        }
+      }
+    }
+  }
+
+  Future<void> _showCallingDialog(String doctorId, String phoneNumber) async {
+    _patientIdController = new TextEditingController();
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Online'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('Enter Patient ID'),
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 0, right: 0),
+                    child: TextFormField(
+                      controller: _patientIdController,
+                      keyboardType: TextInputType.number,
+                      maxLength: 10,
+                      decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        hintText: "Enter Patient ID",
+                        hintStyle:
+                            TextStyle(color: Colors.black54, fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.phone,
+              ),
+              iconSize: 25,
+              color: Colors.blue,
+              splashColor: Colors.purple,
+              onPressed: () {
+                if (_patientIdController.text.isEmpty ||
+                    _patientIdController.text == " " ||
+                    _patientIdController.text == "") {
+                  scaffoldMessenger.showSnackBar(const SnackBar(
+                      content: Text("Please Fill Patient ID Properly")));
+                } else {
+                  _sendCallingInfo(doctorId, phoneNumber,
+                      int.parse(_patientIdController.text), context);
+                }
+                // _showMyDialog(e.id);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
